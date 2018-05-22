63398480b04c8b32aefe6a3acc6f085b1b8b08cc
Debashish Paul <debashish333paul@gmail.com>
2018-05-22 12:11:14 +0600

    BST from cse225

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "binarysearchtree.h"
+#include "binarysearchtree.cpp"
+
+using namespace std;
+
+int main()
+{
+    TreeType<int> p;
+    p.InsertItem(4);
+    p.InsertItem(6);
+    p.InsertItem(8);
+}
+
